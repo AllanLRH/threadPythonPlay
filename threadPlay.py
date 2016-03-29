9fa6a462b4da471124bafce707260af8144025c7
Allan L. R. Hansen <allan.lrh@gmail.com>
2016-03-29 11:56:19 +0200

    Using multiprocessinc.dummy.Pool insted of concurrent.futures

--- threadPlay.py
+++ threadPlay.py
@@ -2,21 +2,20 @@
 # -*- coding: utf8 -*-
 
 import subprocess
-import concurrent.futures
-
+from multiprocessing.dummy import Pool
 
 def get_outout(cmd):
     cmd = cmd.split() if isinstance(cmd, str) else cmd
     stdout = subprocess.check_output(cmd)
-    print(stdout.decode('utf-8'), end='\n'*3)
     stdout = stdout.decode('utf-8')
     return stdout
 
 
 if __name__ == '__main__':
     cmd_list = ['brew cask info adafruit-arduino', 'brew cask info galileo-arduino', 'brew cask info arduino']
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
-        out_res = [executor.submit(get_outout, cmd) for cmd in cmd_list]
-        for out in concurrent.futures.as_completed(out_res):
-            data = out.result()
-            print('≈'*79 + '\n\n' + data)
+    p = Pool(processes=4)
+    out = p.map_async(get_outout, cmd_list)
+    out.wait()
+    if out.successful():
+        for el in out.get():
+            print('≈'*79 + '\n\n' + el)
